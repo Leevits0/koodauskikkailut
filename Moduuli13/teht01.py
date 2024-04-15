ff6135118480432515c7e77e0e6b3a7672d949aa
Leevi <leevislettu@gmail.com>
2024-04-15 15:58:20 +0300

    Mod13 teht01

--- Moduuli13/teht01.py
+++ Moduuli13/teht01.py
@@ -1,28 +1,30 @@
 from flask import Flask, Response, request
 import json
+import math
 
 app = Flask(__name__)
 
-print(__name__)
+def onko_alkuluku(luku):
+    if luku <= 1:
+        return False
+    elif luku == 2:
+        return True
+    elif luku % 2 == 0:
+        return False
+    else:
+        for i in range(3, int(math.sqrt(luku)) + 1, 2):
+            if luku % i == 0:
+                return False
+        return True
 
-
-
-
-
-
-
-
-@app.errorhandler(404)
-def page_not_found(virhekoodi):
-    vastaus = {
-        "status" : "404",
-        "teksti" : "Virheellinen päätepiste"
-    }
-    jsonvast = json.dumps(vastaus)
-    return Response(response=jsonvast, status=404, mimetype="application/json")
+@app.route("/alkuluku")
+def alkuluku():
+    args = request.args
+    luku = float(args.get("luku"))
+    is_prime = onko_alkuluku(luku)
+    result = {"Number": luku, "isPrime": is_prime}
+    return Response(json.dumps(result), status=200, mimetype='application/json')
 
 
 if __name__ == "__main__":
-    app.run(use_reloader=True, host='127.0.0.1',port=3000)
-
-
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
